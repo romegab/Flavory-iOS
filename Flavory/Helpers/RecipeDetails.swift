d294093b4ead69298b230d00468593a2781128d5
romegab <idstoilov@gmial.com>
2021-10-05 22:16:02 +0300

    add some functionality to RecipeDetails

--- Flavory/Helpers/RecipeDetails.swift
+++ Flavory/Helpers/RecipeDetails.swift
@@ -11,15 +11,43 @@ class RecipeDetails{
     
     let regex = try! NSRegularExpression(pattern: "<b>[a-z, 0-9]*</b>")
     
-    var calories: String?
-    var protein: String?
-    var fat: String?
-    var pricePerServing: String?
+    var calories: Int?
+    var protein: Int?
+    var fat: Int?
+    
     
     func loadData( recipeSumary: String){
-        let rawDetails = extractRawDetails(recipeSumary: recipeSumary)
         
-        print(rawDetails)
+        var sentences: [String] = []
+        recipeSumary.enumerateSubstrings(in: recipeSumary.startIndex..., options: .bySentences) { (string, range, enclosingRamge, stop) in
+            if sentences.count < 4{
+                if var sentence = string{
+                    sentence = sentence.replacingOccurrences(of: "<b>", with: "")
+                    sentence = sentence.replacingOccurrences(of: "</b>", with: "")
+                    sentences.append(sentence)
+                }
+            }
+        }
+        print(sentences)
+        
+        let rawDetails = extractRawDetails(recipeSumary: recipeSumary)
+        for detail in rawDetails {
+            if detail.contains("calories") {
+                if let digitValue = Int(detail.components(separatedBy: CharacterSet.decimalDigits.inverted).joined()) {
+                    calories = digitValue
+                }
+            }
+            else if detail.contains("protein"){
+                if let digitValue = Int(detail.components(separatedBy: CharacterSet.decimalDigits.inverted).joined()) {
+                    protein = digitValue
+                }
+            }
+            else if detail.contains("fat"){
+                if let digitValue = Int(detail.components(separatedBy: CharacterSet.decimalDigits.inverted).joined()) {
+                    fat = digitValue
+                }
+            }
+        }
     }
     
     private func extractRawDetails(recipeSumary: String) -> [String] {

11efe1f8e5b9605dd5c4408cc42c46035cfcb0c7
romegab <idstoilov@gmial.com>
2021-10-26 10:48:35 +0300

    Started recipes table view cell ready

---
+++ Flavory/Controllers/StartedRecipeTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  StartedRecipeTableViewCell.swift
+//  Flavory
+//
+//  Created by Ivan Stoilov on 26.10.21.
+//
+
+import UIKit
+
+class StartedRecipeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
